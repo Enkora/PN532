a62b15a7afd85ccefdb2198a5eea820853c0e17a
pjunni <petter.junni@gmail.com>
2021-01-29 18:36:35 +0200

    Extend PN532 with CmacBuffer

--- lib/PN532/PN532.cpp
+++ lib/PN532/PN532.cpp
@@ -13,7 +13,7 @@
 
 #define HAL(func)   (_interface->func)
 
-PN532::PN532(PN532Interface &interface)
+PN532::PN532(PN532Interface &interface) : mi_CmacBuffer(mu8_CmacBuffer_Data, sizeof(mu8_CmacBuffer_Data))
 {
     _interface = &interface;
     mu8_LastAuthKeyNo    = NOT_AUTHENTICATED;
